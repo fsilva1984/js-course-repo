4f9a6543333a56bf97c367d152b34c8c19eae246
fsilva1984 <fsilva916@hotmail.com>
2023-04-05 08:42:04 -0300

    Arch

--- Modules/exercici02/index.js
+++ Modules/exercici02/index.js
@@ -1,6 +1,7 @@
 
 import { states } from "./mod_sys/mod_states.js";
 
+const el = document.querySelector(".op")
 
 
-states()
+states(el)
--- Modules/exercici02/mod_sys/mod_states.js
+++ Modules/exercici02/mod_sys/mod_states.js
@@ -1,8 +1,140 @@
 
-function states() {
+const statesBR = [
+
+  'Acre',
+
+  'Alagoas',
+
+  'Amapá',
+
+  'Amazonas',
+
+  'Bahia',
+
+  'Ceará',
+
+  'Espírito Santo',
+
+  'Goiás',
+
+  'Maranhão',
+
+  'Mato Grosso',
+
+  'Mato Grosso do Sul',
+
+  'Minas Gerais',
+
+  'Pará',
+
+  'Paraíba',
+
+  'Paraná',
+
+  'Pernambuco',
+
+  'Piauí',
+
+  'Rio de Janeiro',
+
+  'Rio Grande do Norte',
+
+  'Rio Grande do Sul',
+
+  'Rondônia',
+
+  'Roraima',
+
+  'Santa Catarina',
+
+  'São Paulo',
+
+  'Sergipe',
+
+  'Tocantins',
+
+  'Distrito Federal',
+
+]
+
+const uf = [
+
+  'AC',
+
+  'AL',
+
+  'AP',
+
+  'AM',
+
+  'BA',
+
+  'CE',
+
+  'ES',
+
+  'GO',
+
+  'MA',
+
+  'MT',
+
+  'MS',
+
+  'MG',
+
+  'PA',
+
+  'PB',
+
+  'PR',
+
+  'PE',
+
+  'PI',
+
+  'RJ',
+
+  'RN',
+
+  'RS',
+
+  'RO',
+
+  'RR',
+
+  'SC',
+
+  'SP',
+
+  'SE',
+
+  'TO',
+
+  'DF',
+
+]
 
 
-  console.log("System of modules successfull");
+function states(element) {
+
+  let el = '';
+
+  for (let i = 0; i < uf.length; i++) {
+
+    el += `<option>${statesBR[i]}</option>`
+  }
+
+  element.innerHTML = `
+  <option selected="">Estado</option>\n
+  ${el}`
+
+}
+
+
+
+function ufBR(y, x){
+
 }
 
 
@@ -11,126 +143,8 @@ export { states }
 
 
 
-/*
 
-AC
 
-AL
 
-AP
 
-AM
 
-BA
-
-CE
-
-ES
-
-GO
-
-MA
-
-MT
-
-MS
-
-MG
-
-PA
-
-PB
-
-PR
-
-PE
-
-PI
-
-RJ
-
-RN
-
-RS
-
-RO
-
-RR
-
-SC
-
-SP
-
-SE
-
-TO
-
-DF
-
-
-
-
-
-
-
-
-
-
-
-Acre
-
-Alagoas
-
-Amapá
-
-Amazonas
-
-Bahia
-
-Ceará
-
-Espírito Santo
-
-Goiás
-
-Maranhão
-
-Mato Grosso
-
-Mato Grosso do Sul
-
-Minas Gerais
-
-Pará
-
-Paraíba
-
-Paraná
-
-Pernambuco
-
-Piauí
-
-Rio de Janeiro
-
-Rio Grande do Norte
-
-Rio Grande do Sul
-
-Rondônia
-
-Roraima
-
-Santa Catarina
-
-São Paulo
-
-Sergipe
-
-Tocantins
-
-Distrito Federal
-
-
-
-*/
