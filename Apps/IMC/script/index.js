74c97c1e6e999cebdf848d969c52118092455ba8
fsilva1984 <fsilva916@hotmail.com>
2023-05-01 15:42:33 -0300

    Windows 11

--- Apps/IMC/script/index.js
+++ Apps/IMC/script/index.js
@@ -1,9 +1,11 @@
+import { pressButton } from "./modal.js";
+
+
+
 const btn = document.querySelector(".btn")
 const peso = document.querySelector("#c-1")
-const info = document.querySelector(".result")
 const altura = document.querySelector("#c-2")
-//const modal = document.querySelector("#c-3")
-const textInfo = document.querySelector(".textInfo")
+const p = document.querySelector("#p-modal")
 
 
 const resp = [
@@ -20,38 +22,41 @@ const resp = [
 
 function calc() {
   const result = peso.value / (altura.value ** 2);
+  const any1 = peso.value
+  const any2 = altura.value
 
-  if (result < 16.9) {
-    textInfo.textContent = `${resp[0]} seu imc: ${result.toFixed(1)}`;
+  if (any1.length !== 0 && any2.length !== 0) {
 
-  } else if (result > 18 && result < 18.4) {
-    textInfo.textContent = `{resp[1]} seu imc: ${result.toFixed(1)}`;
 
-  } else if (result > 18.5 && result < 24.9) {
-    textInfo.textContent = `${resp[2]} seu imc: ${result.toFixed(1)}`;
+    if (result < 16.9) {
+      p.textContent = `${resp[0]} seu imc: ${result.toFixed(1)}`;
 
-  } else if (result > 25 && result < 29.9) {
-    textInfo.textContent = `${resp[3]} seu imc: ${result.toFixed(1)}`;
+    } else if (result > 18 && result < 18.4) {
+      p.textContent = `{resp[1]} seu imc: ${result.toFixed(1)}`;
 
-  } else if (result > 30 && result < 34.9) {
-    textInfo.textContent = `${resp[4]} seu imc: ${result.toFixed(1)}`;
+    } else if (result > 18.5 && result < 24.9) {
+      p.textContent = `${resp[2]} seu imc: ${result.toFixed(1)}`;
 
-  } else if (result > 35 && result < 40) {
-    textInfo.textContent = `${resp[5]} seu imc: ${result.toFixed(1)}`;
+    } else if (result > 25 && result < 29.9) {
+      p.textContent = `${resp[3]} seu imc: ${result.toFixed(1)}`;
 
-  } else { textInfo.textContent = `${resp[6]} seu imc: ${result.toFixed(1)}` }
+    } else if (result > 30 && result < 34.9) {
+      p.textContent = `${resp[4]} seu imc: ${result.toFixed(1)}`;
+
+    } else if (result > 35 && result < 40) {
+      p.textContent = `${resp[5]} seu imc: ${result.toFixed(1)}`;
+
+    } else { p.textContent = `${resp[6]} seu imc: ${result.toFixed(1)}` }
+    
+    
+  }
 }
 
 
 btn.addEventListener("click", () => {
 
+  pressButton()
   calc()
-  info.removeAttribute('hidden')
-
-  setTimeout(() => {
-    info.classList.add('show')
-  }, 8)
-
 
 })
 
